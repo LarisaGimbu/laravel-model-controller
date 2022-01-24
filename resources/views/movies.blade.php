739089101f8f160b3a320c1c278f681ac01a425c
LarisaGimbu <gimbularisa@gmail.com>
2022-01-24 17:46:02 +0100

    movies-cards

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -5,6 +5,27 @@
 
 <div class="container">
   <h1>Movies</h1>
+
+  <div class="movies-container d-flex flex-wrap justify-content-between">
+    @forelse ($movies as $movie)
+      <div class="card mb-3" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">{{$movie['id']}}. {{$movie['title']}}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">{{$movie['original_title']}}</h6>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Nationality: {{$movie['nationality']}} </li>
+            <li class="list-group-item">Date: {{$movie['date']}} </li>
+            <li class="list-group-item">Vote: {{$movie['vote']}} </li>
+          </ul>
+        </div>
+      </div>
+    @empty
+      
+    @endforelse
+  </div>
+  
+  
+
 </div>
   
 @endsection
